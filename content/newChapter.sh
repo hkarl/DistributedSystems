4c26bc7e636789fbb6b8255e0246e59323d5fdf5
Holger Karl <hkarl@ieee.org>
2018-03-23 18:15:42 +0100

    new chapters need symbolic link to Makefile

--- content/newChapter.sh
+++ content/newChapter.sh
@@ -6,3 +6,5 @@ touch $1/$1.org
 echo "#+TITLE: $2" > $1/$1-slides.org
 echo -e "#+include: \"../slidehead.org\"\n\n\\\\begin{frame}\n\\\\titlepage\n\\end{frame}\n\n" >> $1/$1-slides.org
 echo "#+include: \"./$1.org\"" >> $1/$1-slides.org
+
+ln -s ../Makefile $1/Makefile 
