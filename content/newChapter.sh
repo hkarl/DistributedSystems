0fe4b8f3698aee5e759ce0f06fb9e358d82cdd1a
Holger Karl <hkarl@ieee.org>
2018-04-05 15:15:31 +0200

    preparing new chapters for bibliogrpahay

--- content/newChapter.sh
+++ content/newChapter.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 mkdir $1
-touch $1/$1.org
+echo "#+BIBLIOGRAPHY: ../bib plain\n\n" $1/$1.org
 
 echo "#+TITLE: $2" > $1/$1-slides.org
 echo -e "#+include: \"../slidehead.org\"\n\n\\\\begin{frame}\n\\\\titlepage\n\\end{frame}\n\n" >> $1/$1-slides.org
 echo "#+include: \"./$1.org\"" >> $1/$1-slides.org
+echo "\n* References\n *** References\n\\bibliography{../bib}" >> $1/$1-slides.org
 
 ln -s ../Makefile $1/Makefile 
 
